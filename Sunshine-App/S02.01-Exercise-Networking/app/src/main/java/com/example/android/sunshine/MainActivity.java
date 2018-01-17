b8b5cc74268ddec5b92917899e389a8111cf538c
Gil Tolley <34275185+giltolley@users.noreply.github.com>
2018-01-17 15:25:01 -0500

    FINAL
    Completed all TODO items, built the app successfully, and tested it on my Pixel device.

---
+++ Sunshine-App/S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+
+        /*
+         * Using findViewById, we get a reference to our TextView from xml. This allows us to
+         * do things like set the text of the TextView.
+         */
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+        loadWeatherData();
+    }
+
+    private void loadWeatherData() {
+        String preferredLocation = SunshinePreferences.getPreferredWeatherLocation(this);
+
+        URL weatherURL = NetworkUtils.buildUrl(preferredLocation);
+
+        new fetchWeatherTask().execute(weatherURL);
+    }
+
+
+    public class fetchWeatherTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... params)
+        {
+            URL location = params[0];
+            String weatherDataResults = null;
+            try {
+                weatherDataResults = NetworkUtils.getResponseFromHttpUrl(location);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return weatherDataResults;
+        }
+
+        protected void onPostExecute(String weatherDataResults) {
+            mWeatherTextView.setText(weatherDataResults);
+        }
+
+    }
+}
